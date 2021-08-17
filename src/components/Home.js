6221960dbb9dcb3c55704ea8551152411ee56917
joeshep <joeshepmedia@gmail.com>
2021-08-17 13:50:04 -0500

    test edit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Home = () => (
    <>
-        <h2>Nashville Kennels</h2>
+        <h2>Welcome to Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
         <address className="address">
             <div>Visit Us at the Nashville North Location</div>
